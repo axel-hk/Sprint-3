ac700aa43d6720dd6978ff8e5989a9dc7b782a42
Alexander Annenkov <55015984+axel-hk@users.noreply.github.com>
2021-09-08 21:53:45 +0300

    Выполнил дз

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,16 @@
 package ru.sber.nio
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.attribute.BasicFileAttributeView
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.name
+import kotlin.io.path.readText
+import kotlin.io.path.useLines
+import kotlin.streams.toList
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -15,6 +26,17 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
+        val path = "io/result.txt"
+        val result = File(path)
+        val currentPath = File("io/logs").toPath()
 
+        result.outputStream().use { stream ->
+            Files.find(currentPath, 2, { p, _ -> p.toString().endsWith(".log") })
+                .forEach { log -> log.useLines { l -> l
+                .filter { s -> s.contains(subString) }
+                .forEach { l -> stream.write(("${log.name} : ${log.useLines {it.indexOf(l) + 1}} : $l\n").toByteArray()) }
+                }
+            }
+        }
     }
 }
